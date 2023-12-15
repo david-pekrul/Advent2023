a51bf79d96dfc2dfbda8f18024c5d2529f82f8d9
David Pekrul <dbighead77@gmail.com>
2023-12-14 22:45:38 -0600

    Attempting a 'smear' approach, no idea if this is going to work or get anywhere.

--- day12/Day12.scala
+++ day12/Day12.scala
@@ -5,14 +5,22 @@ import helpers.Helpers
 object Day12 {
   def main(args: Array[String]): Unit = {
     val input = Helpers.readFile("day12/test.txt")
+    //    val input = Helpers.readFile("day12/day12.txt")
     val parsed = parse(input)
-    parsed.foreach(println)
+    //    parsed.foreach(println)
 
-    val test1 = parse(Seq("???.### 1,1,3")).head
+    //    val test1 = parse(Seq("???.### 1,1,3")).head
+    //    test1.count()
 
-    test1.count()
+    //    val things = parsed.map(x => (x.count(), x))
+    //    things.foreach(println)
 
 
+//    val test = parse(Seq("???.### 1,1,3")).head
+    val test = parse(Seq("?###???????? 3,2,1")).head
+
+    val testSmear = smear(test.brokenCounts.head,0,test.row)
+    testSmear.foreach(println)
   }
 
   def parse(input: Seq[String]): Seq[SpringRow] = {
@@ -27,53 +35,173 @@ object Day12 {
     })
   }
 
+  //Generate all the groupings to put the input numbers into N buckets
+/*  def generatePartitions(input: Seq[Int], numberOfBuckets: Int) = {
+    if (numberOfBuckets == 1) {
+      //return Seq(input)
+      ???
+    }
+
+    val thing = input.map(num => {
+      (0 until numberOfBuckets).map(idx => {
+        (0 until numberOfBuckets).map(idx2 => {
+          if(idx == idx2){
+            Seq(num)
+          } else {
+            Seq()
+          }
+        }).toSeq
+      }).toSeq
+    })
+
+
+    thing.foreach(println)
+    thing
+  }*/
+
+/*
+  def generatePartitions2(input: Seq[Int], numberOfBuckets: Int) = {
+    val reversedInput = input.reverse
+    //Map[bucketNum -> Seq[Int]]
+    val startEmptyMap = (0 until numberOfBuckets).map(n => n -> Seq[Int]()).toMap
+
+    val built = reversedInput.foldLeft(Seq(startEmptyMap))((acc,nextNumber) => {
+      //Map = Row
+      val something = acc.flatMap(bucketRowMap => {
+        val lastBucketIndex = bucketRowMap.toSeq.sortBy(x => x._1).lastIndexWhere(x => !x._2.isEmpty)
+        val realLastBucket = if (lastBucketIndex == -1){
+          numberOfBuckets
+        } else {
+          lastBucketIndex
+        }
+        realLastBucket
+
+        val updates = (0 until realLastBucket).flatMap(bucketToAddTo => {
+          acc.map(bucketRowMap => {
+            bucketRowMap.updatedWith(bucketToAddTo){
+              case Some(current) => Some(nextNumber +: current)
+            }
+          })
+        })
+        updates
+      })
+      println(something)
+
+      something
+    })
+
+    println(built)
+
+    built
+
+  }*/
+
+
   val trimmingRegex = """^\.*(.+?)\.*$""".r
+  val condensingRegex = """(\.+)"""
 
   def trimString(input: String): Seq[Char] = {
     val trimmingRegex(trim) = input
-    trim.toSeq
+    trim.replaceAll(condensingRegex, ".").toSeq
+  }
+
+  def trimChars(input: Seq[Char]): Seq[Char] = {
+    input.dropWhile(_=='.')
+  }
+
+  def smear(blockSize: Int, startingIndex: Int, symbols: Seq[Char]) = {
+    val symbolsWithIndexAndPoundCount = symbols.zipWithIndex.map(x => (x._1, x._2, Int.MaxValue)).foldLeft((Seq[(Char,Int,Int)](),0))((acc,next) => {
+      val runningTotal = acc._2 + (if(next._1 == '#'){1}else{0})
+      val recomposed = (next._1, next._2, runningTotal)
+      (acc._1 :+ recomposed,runningTotal)
+    })._1
+
+    val rebuilt = symbolsWithIndexAndPoundCount.take(1) ++ symbolsWithIndexAndPoundCount.reverse.sliding(2,1).map(reversedPair => {
+      val right = reversedPair(0)
+      val left = reversedPair(1)
+
+      (right._1, right._2, left._3)
+    }).toSeq.reverse
+
+
+    //Seq[Char symbol, index in string, number of # symbols to the left of this position]
+    println(rebuilt)
+
+    val groupsUntilDotOrTooManyPounds = rebuilt.drop(startingIndex).sliding(blockSize,1).takeWhile(currentGroup => {
+      //does the current group work for the block size?
+      if(currentGroup.exists(charInt => charInt._1 == '.')) {
+        //there is a working spring in the block of broken springs -> can't work
+        false
+      } else if(currentGroup.last._3 == blockSize) {
+        //this cuts it off one too early....
+        false
+      }
+      else {
+        //todo: filter out later if the next char is a #, which would mean this group butts up against another without a gap
+        true
+      }
+    }).toSeq
+
+
+    groupsUntilDotOrTooManyPounds
+    //still need to check if any of the returned options butt up against another # symbol...
   }
 
   case class SpringRow(row: Seq[Char], brokenCounts: Seq[Int]) {
 
-    def count(): Int = {
+    /*def count(): Int = {
 
       def _recCount(currentRow: Seq[Char], remainingBrokenCounts: Seq[Int]): Int = {
 
         if (remainingBrokenCounts.isEmpty) {
-          return 0
+          return 1 // ????
         }
 
         val startState: (SpringRow, SpringRow) = (SpringRow(Seq(), remainingBrokenCounts.take(1)), SpringRow(currentRow, remainingBrokenCounts.tail))
 
         val firstPass = Iterator.iterate(startState)(prevState => {
-          val nextLeft = SpringRow(prevState._1.row :+ prevState._2.row.head, prevState._1.brokenCounts)
-          val nextRight = SpringRow(prevState._2.row.tail, prevState._2.brokenCounts)
-          (nextLeft, nextRight)
-        }).takeWhile(_._2.isEvenPossible()).filter(_._1.isEvenPossible()).toSeq
+          val (prevLeft, prevRight) = prevState
+          if (prevRight.brokenCounts.isEmpty || prevRight.row.isEmpty) {
+            (SpringRow(Seq(), Seq()), SpringRow(Seq(), Seq()))
+          } else {
+            val nextLeft = SpringRow(prevLeft.row :+ prevRight.row.head, prevLeft.brokenCounts)
+            val nextRight = SpringRow(prevRight.row.tail, prevRight.brokenCounts)
+            (nextLeft, nextRight)
+          }
+        })
+          .takeWhile(_._2.isEvenPossible())
+          .filter(_._1.isEvenPossible())
+          .toSeq
 
-        println(firstPass)
-
-        val updatedPass = firstPass.map{ case (left,right) => {
-          val updatedRight = if(right.row.head=='?'){
-            SpringRow(right.row.tail,right.brokenCounts)
+        val updatedPass = firstPass.map { case (left, right) => {
+          val updatedRight = if (right.row.head == '?') {
+            SpringRow(right.row.tail, right.brokenCounts)
           } else {
             right
           }
-          (left,updatedRight)
-        }}.filter(x => x._1.isEvenPossible() && x._2.isEvenPossible())
+          (left, updatedRight)
+        }
+        }.filter(x => x._1.isEvenPossible() && x._2.isEvenPossible())
 
-        println(updatedPass)
+        //        println(updatedPass)
 
-        3
+        updatedPass.map { case (left, right) => {
+          right.count()
+        }
+        }.sum
       }
 
       _recCount(row, brokenCounts)
-      ???
-    }
+    }*/
 
     def isEvenPossible(): Boolean = {
-      if (row.isEmpty && !brokenCounts.isEmpty) {
+      //      if (row.isEmpty && !brokenCounts.isEmpty) {
+      //        return false
+      //      }
+      //      if(row.isEmpty && brokenCounts.isEmpty){
+      //        return false
+      //      }
+      if (row.isEmpty) {
         return false
       }
       if (row.size < brokenCounts.size + brokenCounts.sum - 1) {
